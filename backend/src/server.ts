f6fa077e6a0f182688680435ba057de61f9c6d4f
Liudmyla Makhynia <milam20090929@gmail.com>
2024-11-14 18:15:17 +0200

    add server initialisation  and prismaClient

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,11 +10,9 @@ const PORT = 8000
 app.use("/static/", express.static(path.resolve(__dirname, "./static")))
 app.use(express.json())
 
-app.get("/",(req : Request,res: Response) => {
-    res.render('index')
-})
 
 
-app.listen(PORT,HOST,()=>{
-    console.log("Server is running on http://127.0.0.1:8000")
-})
+
+// app.listen(PORT,HOST,()=>{
+//     console.log("Server is running on http://127.0.0.1:8000")
+// })
