979d68e1caf5203b966082ca908dd4a81af5b7ba
Марія Драгнєва <dragnevamasa95@gmail.com>
2024-11-16 17:42:07 +0200

    Add healthcheck

---
+++ backend/src/core/handlers.ts
@@ -0,0 +1,16 @@
+import {Request, Response} from 'express'
+import {apiVersion} from '../server'
+
+
+class CoreHandlers{
+    healthcheckHandler = (req: Request, res: Response) => {
+      res.json({
+        status: 'available',
+        version: apiVersion
+      })
+    }
+  }
+
+  export const coreHandlers = new  CoreHandlers()
+
+
