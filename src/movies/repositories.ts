e749e0de245d82ceefb459a9bbbb0871c1cb2dc0
AlexeyTarasov77 <AlexeyTarasov77@users.noreply.github.com>
2025-01-10 16:15:30 +0000

    Make code formatting

--- src/movies/repositories.ts
+++ src/movies/repositories.ts
@@ -11,12 +11,14 @@ export class GenresRepository {
 
 export class MoviesRepository {
   async list(): Promise<IMovie[]> {
-    const movies = await prisma.movie.findMany({ include: { genres: { select: { name: true } } } });
+    const movies = await prisma.movie.findMany({
+      include: { genres: { select: { name: true } } },
+    });
     const formattedMovies: IMovie[] = movies.map((movie) => ({
       ...movie,
       genres: movie.genres.map((genre) => genre.name),
     }));
-    return formattedMovies
+    return formattedMovies;
   }
 
   async getOne(id: number): Promise<IMovie> {
